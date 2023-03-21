ce287091dbfbc0b0078165c7a5016abc0a8d218d
Christian Koranteng <korantengchristian@gmail.com>
2023-03-21 19:46:14 +0000

    Upload files

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    const newtask = true;
-    const newtask2 = false;
-    return [newtask, newtask2];
+    task = false;
+    task2 = true;
   }
 
   return [task, task2];
