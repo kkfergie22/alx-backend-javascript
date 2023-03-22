10534bf376ae895cb082bd839199d75945c05c86
Christian Koranteng <korantengchristian@gmail.com>
2023-03-22 12:16:58 +0000

    Task 2

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-restricted-globals, no-undef */
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
