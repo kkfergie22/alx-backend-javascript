2c88ed534ee0b993b1f724bb6ab66fe4b13f6dec
Christian Koranteng <korantengchristian@gmail.com>
2023-03-22 13:23:35 +0000

    Fix linting errors

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,6 +1,5 @@
-/* eslint-disable no-restricted-globals, no-undef */
 export default function getNeighborhoodsList() {
-  this.sanFranciscoNeighborhoods = ["SOMA", "Union Square"];
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   this.addNeighborhood = (newNeighborhood) => {
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
