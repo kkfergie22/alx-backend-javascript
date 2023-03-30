0ee47fee5bb187ee4a77e90a99b2041f87d4e6c3
Christian Koranteng <korantengchristian@gmail.com>
2023-03-30 16:04:16 +0000

    Upload files

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    // eslint-disable-next-line no-constant-condition
+    if (true) {
+      resolve('true');
+    }
+  });
+}
