ff6a2f2e91cecf5de70cdca5f9a4649fea74e95f
Christian Koranteng <korantengchristian@gmail.com>
2023-03-31 15:25:01 +0000

    Fix not returning value

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  promise
+  return promise
     .then(() => {
       console.log('Got a response from the API');
       return { status: 200, body: 'Success' };
     })
-    .catch(() => {
-      return new Error();
-    });
+    .catch(() => Error());
 }
