83635b677f0c99750a69c545c0be745742a89e16
Christian Koranteng <korantengchristian@gmail.com>
2023-03-31 14:44:46 +0000

    update task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -4,5 +4,7 @@ export default function handleResponseFromAPI(promise) {
       console.log('Got a response from the API');
       return { status: 200, body: 'Success' };
     })
-    .catch(() => new Error());
+    .catch(() => {
+      return new Error();
+    });
 }
