34649a52d52d6c20625d6f2b46e25787ac1cae23
Christian Koranteng <korantengchristian@gmail.com>
2023-04-03 15:21:58 +0000

    Upload task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,19 +2,9 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUp = signUpUser(firstName, lastName);
+  const signUP = signUpUser(firstName, lastName);
   const upload = uploadPhoto(fileName);
-  return Promise.all([signUp, upload])
-    .then((results) =>
-      results.map((result) => ({
-        status: 'fulfilled',
-        value: result,
-      }))
-    )
-    .catch((error) => [
-      {
-        status: 'rejected',
-        value: error,
-      },
-    ]);
+  return Promise.allSettled(signUP, upload).then((results) => {
+    console.log(results);
+  });
 }
