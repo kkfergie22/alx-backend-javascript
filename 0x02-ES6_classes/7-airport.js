8c47f9342a9c3d123e242ba4e10f1ae2ec07b672
Christian Koranteng <korantengchristian@gmail.com>
2023-04-04 23:27:20 +0000

    Upload more files

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-underscore-dangle */
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    if (typeof code !== 'string') {
+      throw new Error('Code must be a string');
+    }
+    this._name = name;
+    this._code = code;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
