a5ad9b5d6fead835343317cc414968effb51084d
Christian Koranteng <korantengchristian@gmail.com>
2023-04-11 12:37:01 +0000

    Upload correction of 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,8 @@
 export default function cleanSet(set, startString) {
   const result = [];
+  if (typeof startString !== 'string') {
+    return '';
+  }
   for (const item of set) {
     if (item.startsWith(startString) && startString !== '') {
       result.push(item.replace(startString, ''));
