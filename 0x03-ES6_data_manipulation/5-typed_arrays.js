19ed1fb8e0384cf4ea9f68d9e2e29a31e37e2d00
Christian Koranteng <korantengchristian@gmail.com>
2023-04-11 08:02:11 +0000

    Correct task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const view = new Int8Array(buffer);
-  try {
-    view[position] = value;
-  } catch (error) {
-    return 'Position outside range';
+  if (position >= length || position < 0) {
+    throw new Error('Position outside range');
   }
-
+  view[position] = value;
   const arrayDataView = new DataView(buffer);
   return arrayDataView;
 }
