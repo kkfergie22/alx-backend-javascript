d2c0250534e8ff376b6fb667c14b2e4d0556f56d
Christian Koranteng <korantengchristian@gmail.com>
2023-04-11 13:09:39 +0000

    Fix 8:modify if condition in loop

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   const result = [];
-  if (typeof startString !== 'string' || startString === '') {
+  if (typeof startString !== 'string' || startString === '' || startString === 'undefined' || startString.length === 0) {
     return '';
   }
   for (const item of set) {
