332db1e9a59ef03b36b60b834ac85c96335ed14d
Christian Koranteng <korantengchristian@gmail.com>
2023-04-12 15:33:49 +0000

    Fix error in loop condition

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   const result = [];
-  if (!startString) {
+  if (typeof startString !== 'string' || startString === '' || startString === 'undefined' || startString.length === 0) {
     return '';
   }
   for (const item of set) {
