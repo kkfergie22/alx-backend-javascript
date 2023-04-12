744bc90879a375d0d6899f1d0851097f6715c083
Christian Koranteng <korantengchristian@gmail.com>
2023-04-12 14:55:07 +0000

    Upload advanced

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -4,7 +4,7 @@ export function queryAPI(endpoint) {
   const count = weakMap.get(endpoint) || 0;
   weakMap.set(endpoint, count + 1);
 
-  if (count >= 5) {
+  if (count + 1 >= 5) {
     throw new Error('Endpoint load is high');
   }
 }
