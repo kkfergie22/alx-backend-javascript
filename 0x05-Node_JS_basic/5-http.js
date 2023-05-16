4ff3a81d8dfe215ea95f2e6ca3361d39776f1a0f
Christian Koranteng <korantengchristian@gmail.com>
2023-05-16 14:43:37 +0000

    5-https

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const { countStudents } = require('./3-read_file_async');
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  }
+  if (req.url === '/students') {
+    const filePath = 'database.csv';
+    countStudents(filePath)
+      .then((data) => {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(`This is the list of our students\n${data}`);
+      })
+      .catch((error) => {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end(error.message);
+      });
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
