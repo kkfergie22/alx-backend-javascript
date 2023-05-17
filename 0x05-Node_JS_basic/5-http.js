8b1db4bb9c3a391dd6457ac9d9aa2b2097ecd6f9
Christian Koranteng <korantengchristian@gmail.com>
2023-05-17 10:30:35 +0000

    Update function definition

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,6 +1,26 @@
 const http = require('http');
+
 const fs = require('fs');
-const { countStudents } = require('./3-read_file_async.js');
+
+function countStudents(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, content) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+        return;
+      }
+      const NUMBER_OF_STUDENTS = content.split('\n').slice(1, -1);
+      const fieldSWE = NUMBER_OF_STUDENTS.filter((item) => item.includes('SWE'));
+      const firstNameSWE = NUMBER_OF_STUDENTS.filter((item) => item.includes('SWE')).map((item) => item.split(',')[0]);
+      const fieldCS = NUMBER_OF_STUDENTS.filter((item) => item.includes('CS'));
+      const firstNameCS = NUMBER_OF_STUDENTS.filter((item) => item.includes('CS')).map((item) => item.split(',')[0]);
+      console.log(`Number of students: ${NUMBER_OF_STUDENTS.length}`);
+      console.log(`Number of students in CS: ${fieldCS.length}. List: ${firstNameCS.join(', ')}`);
+      console.log(`Number of students in SWE: ${fieldSWE.length}. List: ${firstNameSWE.join(', ')}`);
+      resolve();
+    });
+  });
+}
 
 const app = http.createServer((req, res) => {
   if (req.url === '/') {
