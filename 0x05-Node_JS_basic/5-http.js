f2b84c4a9cea9c16ecb34eb35e39dbab24804ee4
Christian Koranteng <korantengchristian@gmail.com>
2023-05-17 06:51:42 +0000

    Update 5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,4 +1,5 @@
 const http = require('http');
+
 const { countStudents } = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
@@ -6,17 +7,28 @@ const app = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   }
+
   if (req.url === '/students') {
-    const filePath = 'database.csv';
-    countStudents(filePath)
-      .then((data) => {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(`This is the list of our students\n${data}`);
-      })
-      .catch((error) => {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end(error.message);
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('This is the list of our students\n');
+    return new Promise((resolve, reject) => {
+      const filePath = process.argv[2];
+      fs.readFile(filePath, 'utf8', (err, content) => {
+        if (err) {
+          reject(Error('Cannot load the database'));
+          return;
+        }
+        const NUMBER_OF_STUDENTS = content.split('\n').slice(1, -1);
+        const fieldSWE = NUMBER_OF_STUDENTS.filter((item) => item.includes('SWE'));
+        const firstNameSWE = NUMBER_OF_STUDENTS.filter((item) => item.includes('SWE')).map((item) => item.split(',')[0]);
+        const fieldCS = NUMBER_OF_STUDENTS.filter((item) => item.includes('CS'));
+        const firstNameCS = NUMBER_OF_STUDENTS.filter((item) => item.includes('CS')).map((item) => item.split(',')[0]);
+        console.log(`Number of students: ${NUMBER_OF_STUDENTS.length}`);
+        console.log(`Number of students in CS: ${fieldCS.length}. List: ${firstNameCS.join(', ')}`);
+        console.log(`Number of students in SWE: ${fieldSWE.length}. List: ${firstNameSWE.join(', ')}`);
+        resolve();
       });
+    });
   }
 });
 
