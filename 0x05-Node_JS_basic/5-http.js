becb92b1d93a88079bc4965d111340a64f6a6625
Christian Koranteng <korantengchristian@gmail.com>
2023-05-17 07:51:19 +0000

    Update 5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,33 +1,22 @@
 const http = require('http');
 const fs = require('fs');
+const { countStudents } = require('./3-read_file_async.js');
 
 const app = http.createServer((req, res) => {
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
-  }
-
-  if (req.url === '/students') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('This is the list of our students\n');
-    return new Promise((resolve, reject) => {
-      const filePath = process.argv[2];
-      fs.readFile(filePath, 'utf8', (err, content) => {
-        if (err) {
-          reject(Error('Cannot load the database'));
-          return;
-        }
-        const NUMBER_OF_STUDENTS = content.split('\n').slice(1, -1);
-        const fieldSWE = NUMBER_OF_STUDENTS.filter((item) => item.includes('SWE'));
-        const firstNameSWE = NUMBER_OF_STUDENTS.filter((item) => item.includes('SWE')).map((item) => item.split(',')[0]);
-        const fieldCS = NUMBER_OF_STUDENTS.filter((item) => item.includes('CS'));
-        const firstNameCS = NUMBER_OF_STUDENTS.filter((item) => item.includes('CS')).map((item) => item.split(',')[0]);
-        console.log(`Number of students: ${NUMBER_OF_STUDENTS.length}`);
-        console.log(`Number of students in CS: ${fieldCS.length}. List: ${firstNameCS.join(', ')}`);
-        console.log(`Number of students in SWE: ${fieldSWE.length}. List: ${firstNameSWE.join(', ')}`);
-        resolve();
+  } else if (req.url === '/students') {
+    const filePath = process.argv[2];
+    countStudents(filePath)
+      .then((data) => {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(`This is the list of our students\n${data}`);
+      })
+      .catch((error) => {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end(error.message);
       });
-    });
   }
 });
 
