322981231099f9f84967283c907c49ccf39e3232
Christian Koranteng <korantengchristian@gmail.com>
2023-05-18 07:43:33 +0000

    Upload update

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -20,21 +20,14 @@ function countStudents(filePath) {
   });
 }
 
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    const filePath = process.argv[2].toString();
-    countStudents(filePath)
-      .then((data) => {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(`This is the list of our students\n${data}`);
-      })
-      .catch((error) => {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end(error.message);
-      });
+    const data = await countStudents(process.argv[2]);
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(`This is a list of our students\n${data}`);
   }
 });
 
