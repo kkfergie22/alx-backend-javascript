db0d49c644b16c8ff03981cb3755c3ec189049ce
Christian Koranteng <korantengchristian@gmail.com>
2023-05-18 18:38:37 +0000

    Update functions

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -2,8 +2,11 @@ function calculateNumber(a, b) {
   // if (typeof a !== 'number' || typeof b !== 'number') {
   //   return 'Invalid input';
   // }
+  const round_a = Math.round(a);
+  const round_b = Math.round(b);
+  const sum = round_a + round_b;
 
-  return Math.round(a) + Math.round(b);
+  return sum;
 }
 
 module.exports = calculateNumber;
